8edf7135b0af111bddc96034865101e62768cf08
hamza-ezzahiry <hamza.ezzahiry@gmail.com>
2020-05-28 22:00:16 +0000

    new challenge

---
+++ puzzles/The-power-of-thor-ep1.cpp
@@ -0,0 +1,87 @@
+/**
+ *  *    author: SpiderX
+ *   **/
+
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <stack>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <ctype.h>
+#include <bitset>
+#include <map>
+#include <set>
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define print(x) cout<<x<<'\n'
+#define REP(i,a,b)	for(ll i = a;i < b ;i++)
+#define EPS 1e-7
+#define debug(var) cout << #var << ": " << var << endl;
+
+
+using namespace std;
+
+typedef    unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair <int,int> pi;
+
+/*#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif*/
+
+vector <pi> pt;
+void bresenham(int x1, int y1, int x2, int y2)
+{
+   int m_new = 2 * (y2 - y1);
+   int slope_error_new = m_new - (x2 - x1);
+   for (int x = x1, y = y1; x <= x2; x++)
+   {
+	  cout <<"(" << x << "," << y << ")\n";
+	  pt.PB(MP(x,y));
+	  slope_error_new += m_new;
+
+	  if (slope_error_new >= 0)
+	  {
+		 y++;
+		 slope_error_new  -= 2 * (x2 - x1);
+	  }
+   }
+}
+
+/*
+show_result()
+{
+	if (x == 0 && y == 0)
+
+	else if ()
+}*/
+int main()
+{
+	int lightX; // the X position of the light of power
+	int lightY; // the Y position of the light of power
+	int initialTX; // Thor's starting X position
+	int initialTY; // Thor's starting Y position
+	cin >> lightX >> lightY >> initialTX >> initialTY;
+	bresenham(initialTX, initialTY,lightX, lightY);
+	int r; cin >> r;
+	while(r--)
+	{
+		cout <<pt[1].first - pt[0].first <<" "<<pt[1].second - pt[0].second;
+		pt.erase(pt.begin());
+	}
+	return (0);
+}
+	// Write an action using cout. DON'T FORGET THE "<< endl"
+	// To debug: cerr << "Debug messages..." << endl;
+	// Thor position = (0,0). Light position = (36,17). Energy = 36
